0759111f171bd76a79730c52afc35d8d3a66d44b
N-ig <sarunouenimosannnenn@gmail.com>
2020-05-03 18:29:07 +0900

    Updates

---
+++ precache-manifest.697339585cd61e4a7db3da5de239a4c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7917406fff30c993c99647755f6314",
+    "url": "/iraroji/index.html"
+  },
+  {
+    "revision": "a3e369b3b1b71bdd7200",
+    "url": "/iraroji/static/css/main.8e89bb4e.chunk.css"
+  },
+  {
+    "revision": "f8423ce4510c6aa14615",
+    "url": "/iraroji/static/js/2.c921c125.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iraroji/static/js/2.c921c125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e369b3b1b71bdd7200",
+    "url": "/iraroji/static/js/main.026d2b60.chunk.js"
+  },
+  {
+    "revision": "f482175bf1ab499eff6d",
+    "url": "/iraroji/static/js/runtime-main.bb1b50a2.js"
+  }
+]);
